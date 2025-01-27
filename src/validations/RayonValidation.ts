7075fe5277daafa224ca3a06d178c7079011e275
Joshtri <stuffofyos1516@gmail.com>
2025-01-27 23:47:05 +0800

    enhance createRayonSchema validation with minimum length requirements for fields

--- src/validations/RayonValidation.ts
+++ src/validations/RayonValidation.ts
@@ -1,7 +1,7 @@
 import {z} from "zod";
 
 
-export const createRayonSchema = z.object({ 
-    nama_rayon: z.string(),
-    wilayah: z.string() 
-});
+export const createRayonSchema = z.object({  
+    nama_rayon: z.string().min(1, "Nama rayon harus diisi"),  
+    wilayah: z.string().min(1, "Wilayah harus diisi")  
+  }); 
