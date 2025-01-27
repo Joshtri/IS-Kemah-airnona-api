526e1322da5b5ab37d9dfeaaf74592b2fef51251
Joshtri <stuffofyos1516@gmail.com>
2025-01-27 23:47:42 +0800

    add update and delete routes for Jemaat in JemaatRoute

--- src/routes/JemaatRoute.ts
+++ src/routes/JemaatRoute.ts
@@ -1,8 +1,10 @@
 import {Elysia} from "elysia";
-import { createJemaatHandler, findAllJemaatHandler } from "../controllers/JemaatController";
+import { createJemaatHandler, deleteJemaatHandler, findAllJemaatHandler, updateJemaatHandler } from "../controllers/JemaatController";
 
 export const jemaatRoutes = (app: Elysia)=> {
     app.post("/api/jemaat", createJemaatHandler);
     app.get("/api/jemaat", findAllJemaatHandler);
+    app.put("/api/jemaat/:id", updateJemaatHandler);
+    app.delete("/api/jemaat/:id", deleteJemaatHandler);
 };
 
