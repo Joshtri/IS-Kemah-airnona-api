a6132e2d7f937bc827dff50d4a79df9d69cac046
Joshtri <stuffofyos1516@gmail.com>
2025-01-27 23:44:33 +0800

    refactor Jemaat repository with improved function names and added update/delete functionalities

--- src/repositories/JemaatRepository.ts
+++ src/repositories/JemaatRepository.ts
@@ -1,11 +1,20 @@
 import { PrismaClient } from "@prisma/client";
+import  type { Jemaat, JemaatCreateInput, JemaatUpdateInput } from "../types/jemaat";  
 
 const prisma = new PrismaClient();
 
-export const findAllJemaat = async () => {
-  return await prisma.jemaat.findMany();
-};
+export const findAllJemaatRepo = async (): Promise<Jemaat[]> => {  
+  return await prisma.jemaat.findMany();  
+};  
 
-export const createJemaat = async (data: any) => {
-  return await prisma.jemaat.create({ data });
-};
+export const createJemaatRepo = async (data: JemaatCreateInput) => {  
+  return await prisma.jemaat.create({ data });  
+};  
+
+export const updateJemaatRepo = async (id: number, data: JemaatUpdateInput) => {
+  return await prisma.jemaat.update({ where: { id }, data });
+}
+
+export const deleteJemaatRepo = async (id: number) => {
+  return await prisma.jemaat.delete({ where: { id } });
+}
