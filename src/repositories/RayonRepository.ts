59025b3c65112659516457e444d7b5ba0e308d13
Joshtri <stuffofyos1516@gmail.com>
2025-01-27 23:45:07 +0800

    refactor Rayon repository with improved function names and added update/delete functionalities

--- src/repositories/RayonRepository.ts
+++ src/repositories/RayonRepository.ts
@@ -1,13 +1,26 @@
 import { PrismaClient } from "@prisma/client";
+import type { Rayon, RayonCreateInput, RayonUpdateInput } from "../types/rayon";
 
+const prisma = new PrismaClient();
 
-export const findAllRayon = async () => {
-  const prisma = new PrismaClient();
-  return await prisma.rayon.findMany();
+export const findAllRayonRepo = async () : Promise<Rayon[]> => {
+  // const prisma = new PrismaClient();
+  return await prisma.rayon.findMany({
+    include: {
+      jemaat: true,
+    },
+  });
 };
 
-export const createRayon = async (data: any) => {
-  const prisma = new PrismaClient();
+export const createRayonRepo = async (data: RayonCreateInput) => {
+  // const prisma = new PrismaClient();
   return await prisma.rayon.create({ data });
 };
 
+export const updateRayonRepo = async (id: number, data: RayonUpdateInput) => {
+  return await prisma.rayon.update({ where: { id }, data });
+};
+
+export const deleteRayonRepo = async (id: number) => {
+  return await prisma.rayon.delete({ where: { id } });
+};
