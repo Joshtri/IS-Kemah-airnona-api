75102641234c4bc59a582cba873b00c99d6a7605
Joshtri <stuffofyos1516@gmail.com>
2025-01-27 23:44:53 +0800

    add Kartu Keluarga repository with CRUD operations and improved data handling

--- src/repositories/KartuKeluargaRepository.ts
+++ src/repositories/KartuKeluargaRepository.ts
@@ -1,11 +1,24 @@
 import { PrismaClient } from "@prisma/client";
 
+import type { KartuKeluarga, KartuKeluargaCreateInput, KartuKeluargaUpdateInput } from "../types/kartuKeluarga";
 const prisma = new PrismaClient();
 
-export const findAllKartuKeluarga = async () => {
-  return await prisma.kartuKeluarga.findMany();
+export const findAllKartuKeluargaRepo = async () : Promise<KartuKeluarga[]> => {
+  return await prisma.kartuKeluarga.findMany({
+    include: {
+      jemaat: true,
+    },
+  });
 };
 
-export const createKartuKeluarga = async (data: any) => {
+export const createKartuKeluargaRepo = async (data: KartuKeluargaCreateInput) => {
   return await prisma.kartuKeluarga.create({ data });
+};
+
+export const updateKartuKeluargaRepo = async (id: number, data: KartuKeluargaUpdateInput) => {
+  return await prisma.kartuKeluarga.update({ where: { id }, data });
+};
+
+export const deleteKartuKeluargaRepo = async (id: number) => {
+  return await prisma.kartuKeluarga.delete({ where: { id } });
 };
