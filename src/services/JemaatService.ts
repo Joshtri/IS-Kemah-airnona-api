e6cf992bea1a294c318833eff376680cfd5f557e
Joshtri <stuffofyos1516@gmail.com>
2025-01-27 23:45:52 +0800

    refactor JemaatService to implement CRUD operations with improved type handling

--- src/services/JemaatService.ts
+++ src/services/JemaatService.ts
@@ -1,12 +1,22 @@
 import { createJemaatSchema } from "../validations/JemaatValidation";
-import { createJemaat, findAllJemaat } from "../repositories/JemaatRepository";
+import { createJemaatRepo, findAllJemaatRepo, deleteJemaatRepo, updateJemaatRepo } from "../repositories/JemaatRepository";
+import type { Jemaat, JemaatCreateInput, JemaatUpdateInput } from "../types/jemaat";  
 
-export const addJemaat = async (data: any) => {
+export const addJemaat = async (data: JemaatCreateInput) => {
   const validatedData = createJemaatSchema.parse(data);
-  return await createJemaat(validatedData);
+  return await createJemaatRepo(validatedData);
 };
 
 
-export const findJemaat = async()=>{
-    return await findAllJemaat();
+export const findJemaat = async() : Promise<Jemaat[]> =>{
+    return await findAllJemaatRepo();
 }
+
+export const updateJemaat = async (id: number, data: JemaatUpdateInput): Promise<Jemaat> => {
+  // const validatedData = createJemaatSchema.parse(data);
+  return await updateJemaatRepo(id, data);
+}
+
+export const deleteJemaat = async (id: number): Promise<Jemaat> => {
+  return await deleteJemaatRepo(id);
+};
