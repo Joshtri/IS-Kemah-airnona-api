5adfcd5fcaa51bd7e93a71fc6b55b1e6d3e52342
Joshtri <stuffofyos1516@gmail.com>
2025-01-27 23:45:40 +0800

    refactor RayonService to implement CRUD operations with improved type handling

--- src/services/RayonService.ts
+++ src/services/RayonService.ts
@@ -1,13 +1,22 @@
-import { createRayon, findAllRayon } from "../repositories/RayonRepository";
+import { createRayonRepo, deleteRayonRepo, findAllRayonRepo, updateRayonRepo } from "../repositories/RayonRepository";
+import type { Rayon, RayonCreateInput, RayonUpdateInput } from "../types/rayon";
 import { createRayonSchema } from "../validations/RayonValidation"
 
 
-export const addRayon = async (data: any) => {
+export const addRayon = async (data: RayonCreateInput) => {
     const validateData =  createRayonSchema.parse(data);
-    return await createRayon(validateData);
+    return await createRayonRepo(validateData);
 }
 
-export const findRayon = async () => {
-    return await findAllRayon();
+export const findRayon = async () : Promise<Rayon[]> => {
+    return await findAllRayonRepo();
 }
 
+
+export const updateRayon = async (id: number, data: RayonUpdateInput): Promise<any> => {
+    return await updateRayonRepo(id, data);
+}
+
+export const deleteRayon = async (id: number): Promise<any> => {
+    return await deleteRayonRepo(id);
+}
