b02d619fbf14330aceb9f97271c08f79b89622f3
Joshtri <stuffofyos1516@gmail.com>
2025-01-27 23:47:25 +0800

    enhance createKartuKeluargaSchema validation with minimum length requirements and integer check for jemaat

--- src/validations/KartuKeluargaValidation.ts
+++ src/validations/KartuKeluargaValidation.ts
@@ -1,9 +1,8 @@
 import {z} from "zod";
 
-export const createKartuKeluargaSchema = z.object({
-  nama_kartu_keluarga: z.string(),
-  alamat_keluarga: z.string(),
-  nomor_kartu_keluarga: z.string().max(16),
-  jemaat: z.number()
-
-});
+export const createKartuKeluargaSchema = z.object({  
+  nama_kartu_keluarga: z.string().min(1, "Nama kartu keluarga harus diisi"),  
+  alamat_keluarga: z.string().min(1, "Alamat kartu keluarga harus diisi"),  
+  nomor_kartu_keluarga: z.string().max(16, "Nomor kartu keluarga tidak boleh lebih dari 16 karakter"),  
+  jemaat: z.number().int()  
+});  
