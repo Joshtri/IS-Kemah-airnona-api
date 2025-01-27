09310af55c0d67be878a18c2220e1b7f68ee11e7
Joshtri <stuffofyos1516@gmail.com>
2025-01-27 23:47:54 +0800

    add CRUD routes for Kartu Keluarga in KeluargaRoute

--- src/routes/KeluargaRoute.ts
+++ src/routes/KeluargaRoute.ts
@@ -1,9 +1,11 @@
 import {Elysia} from "elysia";
 
-import { createKartuKeluargaHandler, findallKartuKeluargaHandler } from "../controllers/KartuKeluargaController";
+import { addKartuKeluargaHandler, deleteKartuKeluargaHandler, findAllKartuKeluargaHandler, updateKartuKeluargaHandler } from "../controllers/KartuKeluargaController";
 
 export const kartuKeluargaRoutes =(app:Elysia)=>{
-    app.post('/api/kartu-keluarga', createKartuKeluargaHandler)
-    app.get('/api/kartu-keluarga', findallKartuKeluargaHandler)
+    app.post('/api/kartu-keluarga', addKartuKeluargaHandler);
+    app.get('/api/kartu-keluarga', findAllKartuKeluargaHandler);
+    app.put('/api/kartu-keluarga/:id', updateKartuKeluargaHandler);
+    app.delete('/api/kartu-keluarga/:id', deleteKartuKeluargaHandler);
     
 };
