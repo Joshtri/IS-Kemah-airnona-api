20cc139a6990fe91d9c8f222022e981944a062e3
Joshtri <stuffofyos1516@gmail.com>
2025-01-27 23:48:05 +0800

    enable CORS for all routes in app configuration

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,16 @@ import {Elysia} from "elysia";
 import { jemaatRoutes } from "./routes/JemaatRoute";
 import { kartuKeluargaRoutes } from "./routes/KeluargaRoute";
 import { rayonRoutes } from "./routes/RayonRoute";
+import cors from "@elysiajs/cors";
+
 
 const app = new Elysia();
+
+// Enable CORS for all routes
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"]
+}));
 
 jemaatRoutes(app);
 kartuKeluargaRoutes(app);
