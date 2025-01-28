958bb0ab5220a2841fd9a59f8f4a367058245c52
Joshtri <stuffofyos1516@gmail.com>
2025-01-28 14:12:40 +0800

    add route to retrieve Jemaat by ID

--- src/routes/JemaatRoute.ts
+++ src/routes/JemaatRoute.ts
@@ -1,9 +1,10 @@
 import {Elysia} from "elysia";
-import { createJemaatHandler, deleteJemaatHandler, findAllJemaatHandler, updateJemaatHandler } from "../controllers/JemaatController";
+import { createJemaatHandler, deleteJemaatHandler, findAllJemaatHandler, findJemaatByIdHandler, updateJemaatHandler } from "../controllers/JemaatController";
 
 export const jemaatRoutes = (app: Elysia)=> {
     app.post("/api/jemaat", createJemaatHandler);
     app.get("/api/jemaat", findAllJemaatHandler);
+    app.get("/api/jemaat/:id", findJemaatByIdHandler);
     app.put("/api/jemaat/:id", updateJemaatHandler);
     app.delete("/api/jemaat/:id", deleteJemaatHandler);
 };
