2b89a43b07d6a97664d8466a2b870c5aa13aa202
Joshtri <stuffofyos1516@gmail.com>
2025-01-31 00:03:55 +0800

    update Kartu Keluarga validation schema to rename fields and add phone number requirement

--- src/validations/KartuKeluargaValidation.ts
+++ src/validations/KartuKeluargaValidation.ts
@@ -1,8 +1,8 @@
 import {z} from "zod";
 
 export const createKartuKeluargaSchema = z.object({  
-  nama_kartu_keluarga: z.string().min(1, "Nama kartu keluarga harus diisi"),  
+  nama_kepala_keluarga: z.string().min(1, "Nama kepala keluarga harus diisi"),  
   alamat_keluarga: z.string().min(1, "Alamat kartu keluarga harus diisi"),  
-  nomor_kartu_keluarga: z.string().max(16, "Nomor kartu keluarga tidak boleh lebih dari 16 karakter"),  
-  jemaat: z.number().int()  
+  nomor_telepon_keluarga: z.string().min(1, "Nomor Telepon keluarga tidak boleh kosong"),  
+  // jemaat: z.number().int()  
 });  
